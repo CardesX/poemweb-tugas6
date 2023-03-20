73bfeb26f8a75b6f656673b66d522b5f2672adc7
CardesX <52860177+CardesX@users.noreply.github.com>
2023-03-20 13:56:28 +0700

    Add files via upload

---
+++ Tugas6 191111046_AnggitoRey.php
@@ -0,0 +1,11 @@
+<?php
+    $nama = " Anggito Rey <br>";
+	echo "Nama Lengkap : " . $nama . "<br>";	
+    $alamat = " JL.Klampok Kasri Gg 2C1 336, Malang <br>"; 
+	echo "Alamat : " . $alamat . "<br>";
+    $notelp = "  0895620136638 <br>";
+	echo "Nomor Telepon : " . $notelp . "<br>";
+    $tahun_sekarang =  2020; $tahun_lahir = 1999; 
+    $umur =  $tahun_sekarang - $tahun_lahir; 
+	echo "Umur : " . $umur . "<br>";
+?>
